ed67691db2fe815a535ce32d31ed39942c27a2ff
Bawadywa <o.oxi@ukr.net>
2020-06-04 16:46:05 +0300

    button

---
+++ module3-solution/js/script.js
@@ -0,0 +1,46 @@
+var a_list = document.querySelectorAll("a[href='#']");
+console.log(a_list);
+var span_list = document.querySelectorAll("span[class='icon-bar']");
+var button = document.getElementsByTagName('button');
+console.log(span_list);
+console.log(button);
+var class_bonds = "bonds", class_button = "button", class_span = "span";
+
+function mouse_over_construct(index, flag){
+	if (flag){
+		return function mouse_over(){
+		a_list[index].classList.add(class_bonds);
+		}
+	}else{
+		return function mouse_over(){
+		button[0].classList.add(class_button);
+		span_list[index].classList.add(class_span);
+		}
+	}
+	
+	
+}
+function mouse_out_construct(index, flag){
+	if (flag){
+		return function mouse_out(){
+		a_list[index].classList.remove(class_bonds);
+		}
+	}else{
+		return function mouse_out(){
+		button[0].classList.remove(class_button);
+		span_list[index].classList.remove(class_span);
+		}
+	}
+	
+}
+
+
+for(var i=0; i<span_list.length; i++){
+	span_list[i]["onmouseover"] = mouse_over_construct(i, true);
+	span_list[i]["onmouseout"] = mouse_out_construct(i, true);
+}
+for(i=0; i<a_list.length; i++){
+	a_list[i]["onmouseover"] = mouse_over_construct(i, false);
+	a_list[i]["onmouseout"] = mouse_out_construct(i, false);
+}
+

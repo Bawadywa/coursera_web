d493ee6d2e7f4ca18860ccd338921dbb99efe72f
Bawadywa <o.oxi@ukr.net>
2020-06-04 19:14:04 +0300

    done

--- JS/js/script.js
+++ JS/js/script.js
@@ -4,9 +4,17 @@ function random_num(){
 	return Math.floor(Math.random() * 255);
 }
 
+var i = 0, counter = 10;
 function change_color(){
-	color = random_num().toString(16) + random_num().toString(16) + random_num().toString(16);
-	body[0].style.background = "#"+color;
+	setTimeout(function (){
+		color = random_num().toString(16) + random_num().toString(16) + random_num().toString(16);
+		body[0].style.background = "#"+color;
+		i++;
+		if (i < counter){
+			change_color();
+		}else{
+			i = 0;
+		}
+	}, 30);
 }
-
 body[0]["onclick"] = change_color;

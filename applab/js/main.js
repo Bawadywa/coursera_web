218ceee099d660867d076702b5ea808b4381eef2
Bawadywa <o.oxi@ukr.net>
2021-01-10 19:03:45 +0200

    applab header

---
+++ applab/js/main.js
@@ -0,0 +1,25 @@
+var logo_text = document.querySelector('.logo_text'), logo = document.querySelector('.logo');
+var punch_class = "punch", flag = true;
+
+function animation() {
+	logo_text.classList.add(punch_class);
+	setTimeout(function() {
+		logo_text.classList.remove(punch_class);
+		flag = true
+	}, 1500);
+}
+
+
+function onmouseenter() {
+	if(flag === true){
+		animation();
+		flag = false;
+	}
+	
+
+}
+
+
+logo.addEventListener("mouseenter", function(){
+	onmouseenter();
+})
--- changeable/js/main.js
+++ changeable/js/main.js
@@ -1,5 +1,5 @@
 var body = document.querySelector("body");
-var INDEX = 0, FLAG = true, form_style = "form_style";
+var INDEX = 0, FLAG = true, form_style = "form_style", contact_button = "contact_button";
 var themes = ["purple", "red", "turquoise"];
 
 
@@ -7,8 +7,9 @@ function change(){
 	body.className = themes[INDEX];
 }
 
+
 function run_change(event){
-	if((event.target.classList[1] !== form_style) & (FLAG)){
+	if((event.target.className.indexOf(form_style) === -1) & (event.target.className.indexOf(contact_button) === -1) & (FLAG)){
 		FLAG = false
 		if(INDEX < 2){
 			INDEX += 1;

1cbee88794f6d7f819d5f070183c7b7945b64c3f
Bawadywa <o.oxi@ukr.net>
2021-01-14 00:39:03 +0200

    applab purchase

--- applab/js/main.js
+++ applab/js/main.js
@@ -1,5 +1,5 @@
 var logo_text = document.querySelector('.logo_text'), logo = document.querySelector('.logo');
-var punch_class = "punch", flag = true;
+var punch_class = 'punch', flag = true;
 
 function animation() {
 	logo_text.classList.add(punch_class);
@@ -20,7 +20,39 @@ function onmouseenter() {
 }
 
 
-logo.addEventListener("mouseenter", function(){
+logo.addEventListener('mouseenter', function(){
 	onmouseenter();
 })
 
+
+
+var toggle = document.querySelector('.real_toggle');
+var cards_outer = document.querySelectorAll('.card_outer'), cards_main = document.querySelectorAll('.card_main');
+var rotate_class = 'rotate';
+
+function toggle_switch() {
+
+	function change() {
+		for (var i = 0; i < cards_outer.length; i++) {
+			cards_outer[i].classList.toggle(rotate_class);
+			cards_main[i].classList.toggle(rotate_class);
+		}
+		setTimeout(function(){
+			var cards_price = document.querySelectorAll('.card_price');
+			for (var i = 0; i < cards_price.length; i++) {
+				cards_price[i].innerHTML = "$180";
+		    }
+		}, 300);
+	}
+	change();
+
+	setTimeout(function() {
+		change();
+	}, 500);
+
+
+}
+
+toggle.addEventListener('click', function(){
+	toggle_switch();
+})

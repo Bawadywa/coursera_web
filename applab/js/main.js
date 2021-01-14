8932b2d40f4bd8481a99dfb22382102457ebae9b
Bawadywa <o.oxi@ukr.net>
2021-01-14 23:16:23 +0200

    applab purchase 2

--- applab/js/main.js
+++ applab/js/main.js
@@ -28,7 +28,16 @@ logo.addEventListener('mouseenter', function(){
 
 var toggle = document.querySelector('.real_toggle');
 var cards_outer = document.querySelectorAll('.card_outer'), cards_main = document.querySelectorAll('.card_main');
-var rotate_class = 'rotate';
+var cards_price = document.querySelectorAll('.card_price'), cards_term = document.querySelectorAll('.card_term');
+var cards_button = document.querySelectorAll('.card_button'), cards_titles = document.querySelectorAll('.card_title');
+var cards_desc = document.querySelectorAll('.card_desc'), cards_features = document.querySelectorAll('.card_features');
+var rotate_class = 'rotate', trail = 'trail';
+var price = [['$0', '$99'], ['$180', '$399']],
+	term = [' / month', ' / year'],
+	subscribe = [['Start free trail', 'Subscribe Now'], ['Subscribe Now', 'Subscribe Now']],
+	title = [['Business Class', 'Pro Master'], ['Business', 'Professional']],
+	desc = [['For small teams or office', 'For Best opportunities'], ['For office and small projects', 'For big projects and big teams']]
+var index = 1;
 
 function toggle_switch() {
 
@@ -37,20 +46,30 @@ function toggle_switch() {
 			cards_outer[i].classList.toggle(rotate_class);
 			cards_main[i].classList.toggle(rotate_class);
 		}
-		setTimeout(function(){
-			var cards_price = document.querySelectorAll('.card_price');
-			for (var i = 0; i < cards_price.length; i++) {
-				cards_price[i].innerHTML = "$180";
-		    }
-		}, 300);
+	}
+
+	function change_content() {
+		for (var i = 0; i < cards_outer.length; i++) {
+			card_price = cards_price[i].innerHTML;
+			cards_price[i].innerHTML = price[index][i] +
+			card_price.slice(card_price.indexOf('<')).replace(term[term.length - (1 + index)], term[index]);
+			cards_button[i].innerHTML = subscribe[index][i];
+			cards_titles[i].innerHTML = title[index][i];
+			cards_desc[i].innerHTML = desc[index][i];
+			// cards_features[i].innerHTML += cards_features[i].innerHTML.slice(cards_features[i].innerHTML.indexOf('<span class="card_feature">e'));
+	    }
+
+	    index = Number(!index);
 	}
 	change();
+	setTimeout(function(){
+			change_content();
+	}, 300);
 
 	setTimeout(function() {
 		change();
+		cards_outer[1].classList.toggle(trail);
 	}, 500);
-
-
 }
 
 toggle.addEventListener('click', function(){
